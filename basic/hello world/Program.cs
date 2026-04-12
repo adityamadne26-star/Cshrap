335d1d048497ec56c16c277dcad913d80e2c365a
Aditya Madne <adityamadne26@gmail.com>
2026-04-12 11:11:43 +0530

    Add basic C# program to greet user
    c#

---
+++ basic/hello world/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to C# programming!");
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name);
+    }
+}
